47fff2e3f1039d4306a31c1ed75b8b65b7960318
peroxy </>
2021-10-10 20:54:15 +0200

    Add evolution trigger

--- PokeGuideGenerator/EncounterInfo.cs
+++ PokeGuideGenerator/EncounterInfo.cs
@@ -5,24 +5,24 @@ namespace PokeGuideGenerator
 {
     internal partial class Program
     {
-        public record EncounterInfo(int PokemonId, string Name, string Location, string Version, Encounter Details)
+        public record EncounterInfo(int PokemonId, string Name, string Location, string Version, Encounter Details, string Evolution)
         {
             public override string ToString()
             {
                 if (Details == null)
                 {
-                    return $"{PokemonId};{Name};{Location};{Version};;;;;;";
+                    return $"{PokemonId};{Name};{Location};{Version};;;;;;{Evolution};";
                 }
-                return $"{PokemonId};{Name};{Location};{Version};{string.Join(',', Details?.ConditionValues.Select(x => x.Name))};{Details?.Method.Name};{Details?.Chance}%;{Details?.MinLevel};{Details?.MaxLevel};";
+                return $"{PokemonId};{Name};{Location};{Version};{string.Join(',', Details?.ConditionValues.Select(x => x.Name))};{Details?.Method.Name};{Details?.Chance}%;{Details?.MinLevel};{Details?.MaxLevel};{Evolution};";
             }
 
             public string ToStringWithShortVersion()
             {
                 if (Details == null)
                 {
-                    return $"{PokemonId};{Name};{Location};{PokemonUtil.LongVersionToShort(Version)};;;;;;";
+                    return $"{PokemonId};{Name};{Location};{PokemonUtil.LongVersionToShort(Version)};;;;;;{Evolution};";
                 }
-                return $"{PokemonId};{Name};{Location};{PokemonUtil.LongVersionToShort(Version)};{string.Join(',', Details?.ConditionValues.Select(x => x.Name))};{Details?.Method.Name};{Details?.Chance}%;{Details?.MinLevel};{Details?.MaxLevel};";
+                return $"{PokemonId};{Name};{Location};{PokemonUtil.LongVersionToShort(Version)};{string.Join(',', Details?.ConditionValues.Select(x => x.Name))};{Details?.Method.Name};{Details?.Chance}%;{Details?.MinLevel};{Details?.MaxLevel};{Evolution};";
             }
         }
     }
--- PokeGuideGenerator/Program.cs
+++ PokeGuideGenerator/Program.cs
@@ -15,6 +15,10 @@ namespace PokeGuideGenerator
     internal partial class Program
     {
         //TODO: method to evolve? when to evolve?
+        //we need to lookup the pokemon species first from the existing evolution triggers
+        //-> /pokemon-species/{id}/
+        //-> then /evolution-chain/{id}
+        //then a method that checks which properties are SET and put that into the evolveMethod string for csv
 
         public class Options
         {
@@ -28,19 +32,21 @@ namespace PokeGuideGenerator
         private const string apiEndpoint = "https://pokeapi.co/api/v2";
         private static async Task Main(string[] args)
         {
+            var client = new RestClient(apiEndpoint);
             await Parser.Default.ParseArguments<Options>(args)
-                   .WithParsedAsync(o => GenerateForGeneration(o));
+                   .WithParsedAsync(o => GenerateForGeneration(o, client));
         }
 
-        private static async Task GenerateForGeneration(Options options)
+        private static async Task GenerateForGeneration(Options options, RestClient client)
         {
-            var client = new RestClient(apiEndpoint);
+            var evolutionTriggers = await GetEvolutionTriggers(client);
+            
             var requests = new List<Task<List<EncounterInfo>>>();
             int maxPokedexNumber = PokemonUtil.GetMaxPokedexNumber(options.Generation);
             var versions = PokemonUtil.GetVersions(options.Generation, options.IncludeSideGames);
             for (int i = 1; i <= maxPokedexNumber; i++)
             {
-                requests.Add(GetEncounterInfo(client, i, PokemonUtil.PokedexNumberToName(i), versions));
+                requests.Add(GetEncounterInfo(client, i, PokemonUtil.PokedexNumberToName(i), versions, evolutionTriggers));
             }
 
             var csv = new StringBuilder($"id;name;location;version;conditions;method;chance;minLvl;maxLvl;{Environment.NewLine}");
@@ -49,25 +55,46 @@ namespace PokeGuideGenerator
             foreach (var encounterInfos in encounters)
             {
                 var bestEncounters = GetBestEncountersPerVersion(encounterInfos).ToList();
-                foreach(var encounter in bestEncounters.GroupBy(x => new { x.Location, x.Name, x.PokemonId, x.Details?.Chance, MethodName=x.Details?.Method.Name }))
+                foreach (var encounter in bestEncounters.GroupBy(x => new { x.Location, x.Name, x.PokemonId, x.Details?.Chance, MethodName = x.Details?.Method.Name }))
                 {
                     if (encounter.Count() > 1)
                     {
                         var multipleVersions = string.Join('/', encounter.Select(x => PokemonUtil.LongVersionToShort(x.Version)));
-                        var multipleVersionEncounter = new EncounterInfo(encounter.Key.PokemonId, encounter.Key.Name, encounter.Key.Location, multipleVersions, encounter.First().Details);
+                        var multipleVersionEncounter = new EncounterInfo(encounter.Key.PokemonId, encounter.Key.Name, encounter.Key.Location, multipleVersions, encounter.First().Details, encounter.First().Evolution);
                         csv.AppendLine(multipleVersionEncounter.ToString());
-                    } 
+                    }
                     else
                     {
                         csv.AppendLine(encounter.First().ToStringWithShortVersion());
                     }
                 }
             }
-            
+
             Console.WriteLine("Saving encounters to 'encounters.csv'...");
             File.WriteAllText("encounters.csv", csv.ToString());
-            
+
             Console.WriteLine("Finished!");
+        }
+
+        private static async Task<List<EvolutionTrigger>> GetEvolutionTriggers(RestClient client)
+        {
+            var response = await client.ExecuteGetAsync(new RestRequest("/evolution-trigger"));
+            var triggers = JsonConvert.DeserializeObject<EvoTriggers>(response.Content);
+
+            var requests = new List<Task<IRestResponse>>();
+            foreach (var trigger in triggers.Results)
+            {
+                requests.Add(client.ExecuteGetAsync(new RestRequest($"/evolution-trigger/{trigger.Name}")));
+            }
+
+            var responses = await Task.WhenAll(requests);
+            var evolutionTriggers = new List<EvolutionTrigger>();
+            foreach (var resp in responses)
+            {
+                evolutionTriggers.Add(JsonConvert.DeserializeObject<EvolutionTrigger>(resp.Content));
+            }
+            
+            return evolutionTriggers;
         }
 
         private static IEnumerable<EncounterInfo> GetBestEncountersPerVersion(IEnumerable<EncounterInfo> encounters)
@@ -79,13 +106,15 @@ namespace PokeGuideGenerator
                 .Select(x => x.FirstOrDefault());
         }
 
-        private static async Task<List<EncounterInfo>> GetEncounterInfo(IRestClient client, int pokemonId, string pokemonName, string[] versions)
+        private static async Task<List<EncounterInfo>> GetEncounterInfo(IRestClient client, int pokemonId, string pokemonName, string[] versions, List<EvolutionTrigger> evolutionTriggers)
         {
             Debug.WriteLine($"GET encounters for {pokemonId}-{pokemonName}");
             var response = await client.ExecuteGetAsync(new RestRequest($"/pokemon/{pokemonId}/encounters"));
             var locationAreaEncounters = JsonConvert.DeserializeObject<List<LocationAreaEncounter>>(response.Content);
             var encounters = new List<EncounterInfo>();
-            
+
+            var evolutionTrigger = evolutionTriggers.FirstOrDefault(x => x.PokemonSpecies.Any(y => GetIdFromUrl(y.Url) == pokemonId))?.Name;
+
             foreach (var locationAreaEncounter in locationAreaEncounters)
             {
                 foreach (var version in locationAreaEncounter.VersionDetails)
@@ -94,7 +123,7 @@ namespace PokeGuideGenerator
                     {
                         foreach (var details in version.EncounterDetails)
                         {
-                            encounters.Add(new EncounterInfo(pokemonId, pokemonName, locationAreaEncounter.LocationArea.Name, version.Version.Name, details));
+                            encounters.Add(new EncounterInfo(pokemonId, pokemonName, locationAreaEncounter.LocationArea.Name, version.Version.Name, details, evolutionTrigger));
                         }
                     }
                 }
@@ -102,10 +131,18 @@ namespace PokeGuideGenerator
 
             if (encounters.Count == 0)
             {
-                return new List<EncounterInfo> { new EncounterInfo(pokemonId, pokemonName, "no-location", "none", null)};
+                return new List<EncounterInfo> { new EncounterInfo(pokemonId, pokemonName, "no-location", "none", null, evolutionTrigger) };
             }
             return encounters;
         }
+
+        private static int GetIdFromUrl(string url)
+        {
+            var textId = url.Split('/', StringSplitOptions.RemoveEmptyEntries).Last();
+            return int.Parse(textId);
+        }
+
+        
 
     }
 }
